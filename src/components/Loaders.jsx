5a6339a3e1f7e36debf4391f745f08fc4a330d8c
juan Manuel Silva <juanmanuelsilva06@gmail.com>
2024-04-13 14:18:27 -0300

    cambio de ruta del loader

--- src/components/Loaders.jsx
+++ src/components/Loaders.jsx
@@ -2,8 +2,7 @@ import "../index.css";
 
 const Loader = () => (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center">
-
-        <img src="/public/XboxLogoLoader2s.gif" alt="Loader" />
+        <img src="/XboxLogoLoader2s.gif" alt="Loader" />
     </div>
 );
 
